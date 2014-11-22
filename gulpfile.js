8d7e9903a762c606a5f12f91ee0b7c10a862822b
David Khourshid <davidkpiano@gmail.com>
2014-11-22 14:18:47 -0500

    Initial (sorta) commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass({
+            style: 'expanded'
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('autoprefixer', function() {
+    return gulp.src('css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'], 
+            cascde: false
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass', 'autoprefixer'], function() {
+    gulp.watch('css/*.css', function() {
+        gulp.run('autoprefixer');
+    });
+
+    gulp.watch('scss/*.scss', function() {
+        gulp.run('sass');
+    });
+});
